9c4fcc0281375f30c8a01fca091f6c645c3dbd6f
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-04-27 14:15:12 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 export default function App() {
+
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+
+  const handleSubmit = ()=>{
+    
+    const hei = height / 100;
+    const imc = weight/(hei * hei);
+    
+    if(imc < 18.6){
+      alert("Você está abaixo do peso! " + imc.toFixed(2));
+    }else if(imc >= 18.6 && imc < 24.9){
+      alert("Seu peso é ideal! " +  imc.toFixed(2));
+    }else if(imc >= 24.9 && imc < 34.9){
+      alert("Seu peso é ideal! " +  imc.toFixed(2));
+    }else{
+      alert("Você está acima do peso! " +  imc.toFixed(2));
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Calcule seu IMC</Text>
+      <TextInput
+        style={styles.input}
+        value={weight}
+        onChangeText={(weight) => { setWeight(weight) }}
+        placeholder="Peso(kg)"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        value={height}
+        onChangeText={(height) => { setHeight(height) }}
+        placeholder="Altura (com)"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText} >
+          Calcular
+        </Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-});
+  title: {
+    textAlign: "center",
+    marginTop: 30,
+    fontSize: 30
+
+  },
+  input: {
+    backgroundColor: "#121212",
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: "#FFF",
+    fontSize: 23
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 15,
+    backgroundColor: "#41Aef4",
+    padding: 10,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 25
+  }
+})
